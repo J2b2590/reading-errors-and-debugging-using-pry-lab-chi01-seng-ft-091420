b5cdbdf4f58a965d7ce2d61bff3c949ac8ab6b49
Jeffrey Bolling <j2b2590@gmail.com>
2020-08-20 17:42:13 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -5,8 +5,9 @@ require 'pry'
 def snake_it_up(string)
   if string[0] == "s"
     # binding.pry
-
     10 * "s" + string
+    binding.pry
+    puts "hi"
 
   else
 
@@ -15,5 +16,4 @@ def snake_it_up(string)
 
 end
 
-binding.pry
-puts "hi"
+
