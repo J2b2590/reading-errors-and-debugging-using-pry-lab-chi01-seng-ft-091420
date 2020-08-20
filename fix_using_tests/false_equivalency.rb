c97e5bee953f7b0b2e75454c11db861bd7102d1c
Jeffrey Bolling <j2b2590@gmail.com>
2020-08-20 17:02:20 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -14,11 +14,12 @@ end
 def selection(num)
   if num = 1
      "YUM YUM MUNCH MUNCH MUNCH"
-     binding.pry
+    #  binding.pry
   elsif num = 2
      "HAM HAM HAM IN MY TUMMY"
+     binding.pry
   end
-  binding.pry
+
   puts "Hello World"
 end
 
