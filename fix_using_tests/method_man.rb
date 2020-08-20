72218297fa183127df11603ce3f72adac39a4a3a
Jeffrey Bolling <j2b2590@gmail.com>
2020-08-20 17:27:41 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -5,9 +5,11 @@ def start_game(player1, player2)
   "Hello #{player1} & #{playar2}"
 
 end
-binding.pry
-puts "start_game"
 
 def play_game(player2)
   "#{player1} is better than #{player2}"
 end
+
+
+binding.pry
+puts "start_game"
